eb3703ec890a8c44f8a46d68c1edd717a833e440
Philipp Lackner <mail@pl-coding.com>
2025-04-28 13:37:11 +0200

    Persist default settings

--- app/src/main/java/com/plcoding/echojournal/echos/di/EchosModule.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/di/EchosModule.kt
@@ -4,10 +4,12 @@ import com.plcoding.echojournal.echos.data.audio.AndroidAudioPlayer
 import com.plcoding.echojournal.echos.data.echo.RoomEchoDataSource
 import com.plcoding.echojournal.echos.data.recording.AndroidVoiceRecorder
 import com.plcoding.echojournal.echos.data.recording.InternalRecordingStorage
+import com.plcoding.echojournal.echos.data.settings.DataStoreSettings
 import com.plcoding.echojournal.echos.domain.audio.AudioPlayer
 import com.plcoding.echojournal.echos.domain.echo.EchoDataSource
 import com.plcoding.echojournal.echos.domain.recording.RecordingStorage
 import com.plcoding.echojournal.echos.domain.recording.VoiceRecorder
+import com.plcoding.echojournal.echos.domain.settings.SettingsPreferences
 import com.plcoding.echojournal.echos.presentation.create_echo.CreateEchoViewModel
 import com.plcoding.echojournal.echos.presentation.echos.EchosViewModel
 import com.plcoding.echojournal.echos.presentation.settings.SettingsViewModel
@@ -21,6 +23,7 @@ val echoModule = module {
     singleOf(::InternalRecordingStorage) bind RecordingStorage::class
     singleOf(::AndroidAudioPlayer) bind AudioPlayer::class
     singleOf(::RoomEchoDataSource) bind EchoDataSource::class
+    singleOf(::DataStoreSettings) bind SettingsPreferences::class
 
     viewModelOf(::EchosViewModel)
     viewModelOf(::CreateEchoViewModel)
