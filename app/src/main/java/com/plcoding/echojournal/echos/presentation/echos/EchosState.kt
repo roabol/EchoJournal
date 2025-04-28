869ed4b645a7ad17bdb9b42003279798b8227f41
Philipp Lackner <mail@pl-coding.com>
2025-04-28 12:39:22 +0200

    Display grouped echos

--- app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosState.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosState.kt
@@ -22,10 +22,10 @@ data class EchosState(
     val hasEchosRecorded: Boolean = false,
     val hasActiveTopicFilters: Boolean = false,
     val hasActiveMoodFilters: Boolean = false,
-    val isLoadingData: Boolean = false,
+    val isLoadingData: Boolean = true,
     val recordingState: RecordingState = RecordingState.NOT_RECORDING,
     val moods: List<Selectable<MoodUi>> = emptyList(),
-    val topics: List<Selectable<String>> = listOf("Love", "Happy", "Work").asUnselectedItems(),
+    val topics: List<Selectable<String>> = emptyList(),
     val moodChipContent: MoodChipContent = MoodChipContent(),
     val selectedEchoFilterChip: EchoFilterChip? = null,
     val topicChipTitle: UiText = UiText.StringResource(R.string.all_topics)
